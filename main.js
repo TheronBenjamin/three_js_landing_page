aae0a4933202c873ed3f8609e15662e4180db048
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-03-16 00:54:28 +0100

    added a scene + camera + renderer and change body margin

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 //import './style.css'
-    // Find the latest version by visiting https://cdn.skypack.dev/three.
+// Find the latest version by visiting https://cdn.skypack.dev/three.
   
     import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
   
     const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, innerWidth/innerWidth, 0.1, 1000)
+    const renderer = new THREE.WebGLRenderer()
 
 
     console.log(scene)
+    console.log(camera)
+    console.log(renderer)
+
+    renderer.setSize(innerWidth, innerHeight)
+    document.body.appendChild(renderer.domElement);
