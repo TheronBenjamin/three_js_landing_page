ac73706ba8ddb4854d082d31db708826b11aabcb
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-03-16 03:00:53 +0100

    add gui module to promute interactivity with the user

--- main.js
+++ main.js
@@ -2,11 +2,52 @@
 // Find the latest version by visiting https://cdn.skypack.dev/three.
   
     import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
-  
+    import * as dat from 'dat.gui'
+
+    // GUI console
+
+    const gui = new dat.GUI()
+    const world = {
+        plane:{
+            width: 10,
+            height: 10,
+            widthSegments: 10,
+            heightSegments: 10
+        }
+    }
+    gui.add(world.plane, "width", 1, 20).onChange(generatePlane)
+    gui.add(world.plane, "height", 1, 20).onChange(generatePlane)
+    gui.add(world.plane, "widthSegments", 1, 200).onChange(generatePlane)
+    gui.add(world.plane, "heightSegments", 1, 200).onChange(generatePlane)
+
+    function generatePlane() {
+        planeMesh.geometry.dispose();
+        planeMesh.geometry = new THREE.
+        PlaneGeometry(
+            world.plane.width,
+            world.plane.height,
+            world.plane.widthSegments,
+            world.plane.heightSegments
+            )
+        const {array} = planeMesh.geometry.attributes.position;
+        for (let i=0; i<array.length; i+=3){
+        const x = array[i]
+        const y = array[i+1]
+        const z = array[i+2]
+        array[i+2] = z + Math.random()
+        }
+    }
+
+
     // Set scene camera and renderer properties
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, innerWidth/innerWidth, 0.1, 1000)
+    const camera = new THREE.PerspectiveCamera(
+        75, 
+        innerWidth/innerWidth, 
+        0.1, 
+        1000
+        )
     const renderer = new THREE.WebGLRenderer()
 
     //console.log(scene)
